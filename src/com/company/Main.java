46a74171bb917dac2f662ef8dee1a5b699ae074d
p-ganesh <41019754+p-ganesh@users.noreply.github.com>
2018-10-17 21:54:25 -0400

    big benny

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        Scanner kbreader = new Scanner(System.in);
+
+        System.out.println("Enter your word.");
+        String input = kbreader.next();
+        input.toUpperCase();
+
+        if (input == "Exit") {
+            System.exit(0);
+        }
+
+        else {
+            String finish = input + "extra";
+
+            String sp [] = new String[finish.length()];
+            sp = finish.split("as");
+            System.out.println(sp.length - 1);
+        }
+
+}}
